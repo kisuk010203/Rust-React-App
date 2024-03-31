f5ef05fdc2ef83926a8062ef0141b41069f132eb
kisuk010203 <chriskisuk@gmail.com>
2024-04-01 00:30:39 +0900

    Make Component AppRoutes in a separate file

---
+++ frontend/src/routes.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./containers/home-page";
+import { Todos } from "./containers/todo-page";
+import { GraphQLPage } from "./containers/graphql-page";
+import { AccountPage } from "./containers/account-page";
+import { ActivationPage } from "./containers/activation-page";
+import { LoginPage } from "./containers/login-page";
+import { RecoveryPage } from "./containers/recovery-page";
+import { RegistrationPage } from "./containers/registration-page";
+import { ResetPage } from "./containers/reset-page";
+
+export const AppRoutes: React.FC = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/todos" element={<Todos />} />
+            <Route path="/gql" element={<GraphQLPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/recovery" element={<RecoveryPage />} />
+            <Route path="/reset" element={<ResetPage />} />
+            <Route path="/activate" element={<ActivationPage />} />
+            <Route path="/register" element={<RegistrationPage />} />
+            <Route path="/account" element={<AccountPage />} />
+        </Routes>
+    );
+}
